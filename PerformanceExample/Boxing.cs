afec258a4a7e1bcb433103be13066faa4ac4cad9
vmussak <vinicius.mussak@outlook.com>
2015-04-11 15:29:42 -0300

    First Commit

---
+++ PerformanceExample/Boxing.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace PerformanceExample
+{
+    public static class Boxing
+    {
+        public static void Comparison()
+        {
+            var time = new Stopwatch();
+            time.Start();
+
+            ArrayList arrayList = new ArrayList(20000000);
+            for (int i = 0; i < 20000000; i++)
+                arrayList.Add(i);
+
+            time.Stop();
+            Console.WriteLine("ArrayList com 20.000.000 itens:");
+            Console.WriteLine("Tempo: {0}", time.Elapsed);
+            Console.WriteLine("");
+
+            time.Restart();
+
+            List<int> intList = new List<int>(20000000);
+            for (int i = 0; i < 20000000; i++)
+                intList.Add(i);
+
+            time.Stop();
+            Console.WriteLine("List<int> com 20.000.000 itens:");
+            Console.WriteLine("Tempo: {0}", time.Elapsed);
+        }
+    } 
+} 
+ 
+
+
+
+
+
---
+++ PerformanceExample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PerformanceExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boxing.Comparison();
+            Console.ReadKey();
+        }
+    }
+}
